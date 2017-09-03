3be3d80841851b1a633911f6c3cfcd106062261a
Lab2Ies <artpal2014@gmail.com>
2017-09-03 14:23:28 -0600

    domingo4

--- consulta.php
+++ consulta.php
@@ -1,8 +1,15 @@
 <?php
 
-var_dump($_POST);
-var_dump($_GET);
+//var_dump($_POST);
+//var_dump($_GET);
 
 require_once 'driver.php';
 
 $enlace = new DMysqli();
+$estado = $enlace->query("insert into cliente VALUES(DEFAULT, 'Maria','Managua',30) ");
+if ($estado == true){
+    echo 'Exito agregando datos';
+}
+else {
+    echo 'Fallo al agregar datos';
+}
